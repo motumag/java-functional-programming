f71141578ea57f322cf62f5efdd8a7963cd5a934
motumag <motumagishu27@gmail.com>
2024-06-23 22:02:27 +0300

    for all orderid and specific id is done.

--- FilterWithStream.java
+++ FilterWithStream.java
@@ -23,9 +23,22 @@ public class FilterWithStream {
     public static void main(String[] args) {
         List<OrderPlacing> listOfOrder = new ArrayList<OrderPlacing>();
         listOfOrder.add(new OrderPlacing(800.00, 5, 1));
-        listOfOrder.add(new OrderPlacing(1000.00, 3, 2));
+        listOfOrder.add(new OrderPlacing(1000.00, 3, 1));
         listOfOrder.add(new OrderPlacing(400.00, 7, 3));
-        listOfOrder.add(new OrderPlacing(200.00, 8, 4));
+        listOfOrder.add(new OrderPlacing(200.00, 8, 1));
+        // if you want to add the total price of the order
+        double sumOfPrices = listOfOrder.stream()
+                .filter(p -> p.price > 500)
+                .mapToDouble(p -> p.price)
+                .sum();
+        System.out.println("All price: " + sumOfPrices);
+        // for specific orderId
+        int specificOrderId = 1;
+        double sumOfPricesForSpecificOrderId = listOfOrder.stream()
+                .filter(p -> p.orderId == specificOrderId)
+                .mapToDouble(p -> p.price)
+                .sum();
+        System.out.println("price for id : " + specificOrderId + ": is: " + sumOfPricesForSpecificOrderId);
         List<Double> orderPriceList = listOfOrder.stream().filter(p -> p.price > 500).map(p -> p.price)
                 .collect(Collectors.toList());
         System.out.println("List of orderPrice are: " + orderPriceList);
